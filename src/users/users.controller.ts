df02c063ed7f3551ff01f26e3b58e43457f0e6fc
IjonK007 <kushta.joni@gmail.com>
2024-08-27 13:26:03 +0200

    connected services logic to controllers

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,29 +1,32 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+
     @Get()
     findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
-        return [];
+        return this.usersService.findAll(role);
     }
 
     @Get(':id')
-    findOne(@Param('id') id: number) {
-        return { id };
+    findOne(@Param('id') id: string) {
+        return this.usersService.findOne(+id);
     }
 
     @Post()
-    create(@Body() user: {}) {
-        return user;
+    create(@Body() user: {name: string, email: string, role: 'INTERN' | 'ENGINEER' | 'ADMIN'}) {
+        return this.usersService.create(user);
     }
      
     @Patch(':id')
-    update(@Param('id') id: number, @Body() userUpdate: {}) {
-        return { id, ...userUpdate };
+    update(@Param('id') id: string, @Body() userUpdate: {name?: string, email?: string, role?: 'INTERN' | 'ENGINEER' | 'ADMIN'}) {
+        return this.usersService.update(+id, userUpdate);
     }
 
     @Delete(':id')
-    delete(@Param('id') id: number) {
-        return { id };
+    delete(@Param('id') id: string) {
+        return this.usersService.delete(+id);
     }
 }
