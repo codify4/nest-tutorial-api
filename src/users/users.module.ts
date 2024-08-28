748aa0b9dddd6b7b666d1a4fd48729241c72ea74
codify4 <kushta.joni@gmail.com>
2024-08-29 00:29:53 +0200

    added authorization middleware for endpoints

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { ValidateUserMiddleware } from './middlewares/validate-user.middleware';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService]
 })
-export class UsersModule {}
+export class UsersModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(ValidateUserMiddleware)
+      .forRoutes('*');
+  }
+}
